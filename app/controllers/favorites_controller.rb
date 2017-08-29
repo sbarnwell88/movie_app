b0033df52a2c365cd0bd7f3d53d77622e4a0c3fd
sbarnwell88 <susy6688@gmail.com>
2017-08-29 19:29:26 -0400

    redid add to fave button

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,14 @@
 class FavoritesController < ApplicationController
     def index
-        @favorites = Favorites.all
+        @favorites = Favorite.all
     end
     
     def create
         film_id = params[:film_id]
-        user_id = params[:user_id]
-        Favorite.create(user_id: user_id, film_id: film_id)
+        # user_id = params[:user_id]
+        @favorite = Favorite.create!(user_id: current_user.id, film_id: film_id )
+
+        redirect_to favorites_path(@favorite)
     end
 
 end
