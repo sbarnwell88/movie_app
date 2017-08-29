4f1a1ebb79b9f19810b4ed17624d86e34c13a7c4
sbarnwell88 <susy6688@gmail.com>
2017-08-29 15:34:38 -0400

    created film successfully

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,2 +1,20 @@
 class FilmsController < ApplicationController
+    def index
+        @films = Film.all
+    end
+
+    def new
+        @film = Film.new
+    end
+
+    def create
+        @film = Film.create!(film_params)
+
+        redirect_to films_path(@film)
+    end
+
+      private
+    def film_params
+        params.require(:film).permit(:title, :genre, :year, :synopsis, :picture)
+    end
 end
